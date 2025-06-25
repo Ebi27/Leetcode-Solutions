f0c4031c2f23a6364ed07a559b39a84fbf9a8916
Ebi Kpemi-Ogokimi <kpemiogokimiebi@gmail.com>
2025-06-25 15:53:43 -0400

    Time: 0 ms (100%), Space: 12.7 MB (60.02%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.py
+++ 2058-concatenation-of-array/2058-concatenation-of-array.py
@@ -4,9 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        ans = nums[:] * 2 # Create a copy of nums and replicate it twice using the * operator. The time complexity is O(2n)
-        return ans
+        #ans = nums[:] * 2 # Create a copy of nums and replicate it twice using the * operator. The time complexity is O(2n)
+        return nums * 2
 
         """
-        If we want to save on space, we can just return the operation directly (return nums[:] * 2 )
+        If we want to save on space, we can just return the operation directly (return nums * 2 )
         """
